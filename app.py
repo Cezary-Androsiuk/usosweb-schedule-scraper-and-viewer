b624d05e858a26e6fb40e2e9cfde2dc89dccfb91
Cezary Androsiuk PC <czandros@wp.pl>
2025-11-14 19:14:09 +0100

    added reusing existing images of schedule

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, Response
+import json
+from pathlib import Path
+from datetime import datetime, timedelta
 
 # from old_uwb_connector import asyncio, update_cookies, download_schedule_image, schedule_image_name
-from uwb_connector import download_schedule_safe
+from uwb_connector import download_schedule_safe, schedule_image_name
 from date_validator import todays_week, to_week_date, week_range, week_forward, week_backwards
 
 app = Flask(__name__)
@@ -42,9 +45,39 @@ def root():
         next_week=next_week)
     
 
+def is_file_new_enough(image_name, seconds=10*60):
+    file_path = Path(image_name)
+
+    now = datetime.now()
+    max_age_delta = timedelta(seconds=seconds)
+    cutoff_time = now - max_age_delta
+
+    if file_path.is_file():
+        file_mod_timestamp = file_path.stat().st_mtime
+        file_mod_time = datetime.fromtimestamp(file_mod_timestamp)
+        if file_mod_time >= cutoff_time:
+            return True
+        else:
+            return False
+    else:
+        return False # if not exist also return false
+
 
 @app.route('/stream-data/<schedule_date>')
 def stream_data(schedule_date):
+    # check if image existing image is not older than 10min, if so, it can be used
+
+    image_name = schedule_image_name(schedule_date)
+
+    if is_file_new_enough(image_name=image_name, seconds=10*60):
+        def exisiting_image_stream(image_name):
+            message = {'type': 'link', 'payload': f'{image_name}'}
+            yield f'data: {json.dumps(message)}\n\n'
+        
+        return Response(exisiting_image_stream(image_name=image_name), mimetype='text/event-stream')
+    
+
+
     return Response(download_schedule_safe(schedule_date), mimetype='text/event-stream')
 
 
